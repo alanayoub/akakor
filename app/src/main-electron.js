db62372d921b9a71936e0982fd66d12ec5ebc233
Alan Ayoub <alanayoub@gmail.com>
2017-04-22 21:03:14 +0100

    Remember window size and location

--- app/src/main-electron.js
+++ app/src/main-electron.js
@@ -1,19 +1,34 @@
-var electron = require('electron');
-var app = electron.app;
-var BrowserWindow = electron.BrowserWindow;
-var path = require('path');
-var url = require('url');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+const windowStateKeeper = require('electron-window-state');
 
 function createWindow({local, web}) {
 
+    // Load the previous window state with fallback to defaults
+    let mainWindowState = windowStateKeeper({
+        defaultWidth: 1000,
+        defaultHeight: 800
+    });
+
     let win = new BrowserWindow({
-        width: 2000,
-        height: 1024,
+        x: mainWindowState.x,
+        y: mainWindowState.y,
+        width: mainWindowState.width,
+        height: mainWindowState.height,
         'web-preferences': {
             'web-security': true,
             'allowRunningInsecureContent': false
         }
     });
+
+    // Register listeners on the window, so we can update the state
+    // automatically (the listeners will be removed when the window is closed)
+    // and restore the maximized or full screen state
+    mainWindowState.manage(win);
+
     const ses = win.webContents.session;
 
     if (local) {
