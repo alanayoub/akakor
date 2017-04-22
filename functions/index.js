052c91da196777df8d1c7fff6fe2d2442746568e
Alan Ayoub <alanayoub@gmail.com>
2017-04-22 10:47:26 +0100

    Adding firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
-var functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+'use strict';
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Copy items posted to configurations_public/uid to configurations_public_read
+//
+exports.sync_public_configs = functions.database
+    .ref('/configurations_public/{uid}/{config_id}').onWrite(event => {
+        const message = event.data.val();
+        const item = {};
+        const valid_fields = [
+            'author_id',
+            'author_display_name',
+            'author_photo_url',
+            'title',
+            'description',
+            'date_created',
+            'version',
+            'layout'
+        ];
+        valid_fields.forEach(field => {
+            if (message[field] !== void 0) item[field] = message[field];
+        });
+        admin.database().ref('/configurations_public_read').push(item).then(snapshot => {
+            console.log('added new item', snapshot);
+        });
+    });
